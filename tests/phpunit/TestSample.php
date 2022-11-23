efbe212cac65c7dc6703c778817e0b2a9f90362c
tanmayjay <jktanmay@gmail.com>
2022-11-24 01:20:06 +0600

    Added initial development setup

---
+++ tests/phpunit/TestSample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Jblog_Contributors
+ */
+
+/**
+ * Sample test case.
+ */
+class TestSample extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
